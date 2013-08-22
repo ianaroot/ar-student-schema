1b0c1eac87815b797c2f4ccc18d90267bfb2e8f4
Dan Apczynski <dan_apczynski@Dans-MacBook-Pro.local>
2013-08-21 19:54:34 -0700

    Initial commit

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,33 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  attr_reader :birthday
+  
+  def assign_attributes(hash)
+    @first_name = hash[:first_name]
+    @last_name = hash[:last_name]
+    @gender = hash[:gender]
+    @birthday = hash[:birthday]
+    @email = hash[:email]
+    @phone = hash[:phone]
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    (now.year - @birthday.year - (@birthday.to_date.change(:year => now.year) > now ? 1 : 0))
+  end
+
 end
+
+student = Student.new
+student.assign_attributes(
+      :first_name => "Happy",
+      :last_name => "Gilmore",
+      :gender => 'male',
+      :birthday => Date.new(1970,9,1))
+p student.name
+p student.age
