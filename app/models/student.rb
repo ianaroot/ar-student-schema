ecf1ea52c41f214c0ceb5ef331e17f0802183f38
Dan Apczynski <dan_apczynski@Dans-MacBook-Pro.local>
2013-08-22 09:31:12 -0700

    Got 'validations' rspec to pass

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,34 +1,51 @@
 require_relative '../../db/config'
+# require 'activerecord'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
-  attr_reader :birthday
-  
-  def assign_attributes(hash)
-    @first_name = hash[:first_name]
-    @last_name = hash[:last_name]
-    @gender = hash[:gender]
-    @birthday = hash[:birthday]
-    @email = hash[:email]
-    @phone = hash[:phone]
-  end
+  validates :email, format: { with: /\A[\w]+@[\w]+.[\w]{2,}.?[\w]*\z/, message: "email format must be valid" }
+  validates :email, uniqueness: true
+  validates :age, numericality: { greater_than_or_equal_to: 5 }
+  validates :phone, format: { with: /\A\(?\d{3}\)?[\s-]?\d{3}-?\d{4}\s?x?\d*\z/, message: "phone number must be valid"}
+
 
   def name
-    "#{@first_name} #{@last_name}"
+    "#{first_name} #{last_name}"
   end
 
   def age
-    now = Time.now.utc.to_date
-    (now.year - @birthday.year - (@birthday.to_date.change(:year => now.year) > now ? 1 : 0))
+    now = Date.today
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
   end
 
+  def validate_email
+
+  end
 end
 
 student = Student.new
 student.assign_attributes(
       :first_name => "Happy",
       :last_name => "Gilmore",
+      :email => "happy@gilmore.com",
       :gender => 'male',
       :birthday => Date.new(1970,9,1))
-p student.name
-p student.age
+p student.valid?
+
+student2 = Student.new
+student2.assign_attributes(
+      :first_name => "Dave",
+      :last_name => "Grohl",
+      :email => "happy@gilmore.com",
+      :gender => 'male',
+      :birthday => Date.new(1939,1,3))
+p student2.valid?
+
+student3 = Student.new
+student3.assign_attributes(
+      :first_name => "Trey",
+      :last_name => "Anastasio",
+      :email => "trey@phish.com",
+      :gender => 'male',
+      :birthday => Date.new(1912,1,3))
+p student3.valid?
